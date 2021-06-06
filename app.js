8b0170aeaefaf8db4438cd706e5d1514ab34f35a
sanjeevyg <yogi.sjv@gmail.com>
2021-06-05 20:56:44 -0500

    db updated

--- app.js
+++ app.js
@@ -1,32 +1,38 @@
 const express = require('express');
 const app = express();
-const mongoose = require('mongoose');
-mongoose.connect = 
+// const mongoose = require('mongoose');
+// mongoose.connect = 
 
-app.use('/posts', () => {
-    console.log('This is a middleware running!')
-})
+// app.use('/posts', () => {
+//     console.log('This is a middleware running!')
+// })
 
-
-app.get('/', (request, response) => {
-    response.send('We are home!')
-})
-
-app.get('/posts', (request, response) => {
-    response.send('We are on post!')
-})
-app.listen(3000)
+// const port = 3000
 
 
+// app.get('/', (request, response) => {
+//     response.send('We are home!')
+// })
 
-const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://yogi:mC7yuJBnxiMXpF20@cluster0.nen5a.mongodb.net/node-mongodb-05-Jun-21?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+// app.get('/posts', (request, response) => {
+//     response.send('We are on post!')
+// })
+
+
+// app.listen(3000, () => {
+//     console.log(`listening to port ${port}`)
+// })
+
+
+
+// const MongoClient = require('mongodb').MongoClient;
+// const uri = "mongodb+srv://yogi:mC7yuJBnxiMXpF20@cluster0.nen5a.mongodb.net/node-mongodb-05-Jun-21?retryWrites=true&w=majority";
+// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+// client.connect(err => {
+//   const collection = client.db("test").collection("devices");
+//   // perform actions on the collection object
+//   client.close();
+// });
 
 
 // const password = mC7yuJBnxiMXpF20
--- server.js
+++ server.js
@@ -1,17 +1,35 @@
+const fs = require('fs')
+
+// const textIn = fs.readFileSync('./input.txt', 'utf-8')
+// console.log(textIn)
+
+// const textOut = `This s what we know about he avocado: ${textIn}.\nCreated on ${Date.now()}`;
+// fs.writeFileSync('./output.txt', textOut);
+// console.log('File written')
+
+const express = require('express')
+const app = express()
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-const app = require('./app');
+// const app = require('./app.js');
 
 dotenv.config({ path: './config.env' })
 
 const DB = process.env.DATABASE.replace(
-    '<PASSWORD',
+    '<PASSWORD>',
     process.env.DATABASE_PASSWORD
 );
 
-mongoose.connect()
-  const port = process.env.PORT || 3000;
 
-  app.listen(port, () => {
-      console.log(`App running on port ${port}...`)
-  });
+mongoose.connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+}).then(() => console.log("DB connection succesful!"))
+const port = process.env.PORT || 3000;
+// const port = 3000 
+
+app.listen(port, () => {
+    console.log(`App running on port ${port}...`)
+});
