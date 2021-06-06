026f187a7ac861baeef1a56cf57416db2cb3f6a9
sanjeevyg <yogi.sjv@gmail.com>
2021-06-05 19:42:59 -0500

    mongodb database created

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-
+mongoose.connect = 
 
 app.use('/posts', () => {
     console.log('This is a middleware running!')
@@ -17,3 +17,18 @@ app.get('/posts', (request, response) => {
 })
 app.listen(3000)
 
+
+
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://yogi:mC7yuJBnxiMXpF20@cluster0.nen5a.mongodb.net/node-mongodb-05-Jun-21?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
+
+// const password = mC7yuJBnxiMXpF20
+
+// mongodb+srv://yogi:<password>@cluster0.7c31p.mongodb.net/test
---
+++ server.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const app = require('./app');
+
+dotenv.config({ path: './config.env' })
+
+const DB = process.env.DATABASE.replace(
+    '<PASSWORD',
+    process.env.DATABASE_PASSWORD
+);
+
+mongoose.connect()
+  const port = process.env.PORT || 3000;
+
+  app.listen(port, () => {
+      console.log(`App running on port ${port}...`)
+  });
