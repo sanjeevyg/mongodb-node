9af908d9d26fc2e5046568ca2d129b68d4ab33a9
sanjeevyg <yogi.sjv@gmail.com>
2021-06-20 19:49:35 -0500

    getAllUser method udpated

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,14 +1,19 @@
 const User = require('./../models/userModel.js');
 
-exports.getAllUsers = (req, res) => {
-    const user = User.find()
+exports.getAllUsers = async (req, res) => {
+    try { const user = await User.find()
     res.status(500).json({
         status: 'error',
         message: 'This route is not yet defined',
         data: {
             user
         }
-    })
+    })} catch(err) {
+        res.status(400).json({
+            status: 'fail',
+            message: 'Invalid data sent!'
+    }
+    
 }
 
 exports.deleteUser = (req, res) => {
