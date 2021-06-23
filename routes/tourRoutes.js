e58559d0eacecf55c8aa6eaa538267c13c6458f9
sanjeevyg <yogi.sjv@gmail.com>
2021-06-22 22:19:48 -0500

    tourRoutes updated L

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -3,18 +3,18 @@ const tourController = require('./../controllers/tourController');
 
 const router = express.Router();
 
-// router.param('id', tourController.checkID);
+router.param('id', tourController.checkID);
 
 router  
     .route('/')
     .get(tourController.getAllTours)
     .post(tourController.createTour);
 
-// router
-//     .route('/:id')
-//     .get(tourController.getTour)
-//     .patch(tourController.udpateTour)
-//     .delete(tourController.deleteTour)
+router
+    .route('/:id')
+    .get(tourController.getTour)
+    .patch(tourController.udpateTour)
+    .delete(tourController.deleteTour)
 
 
 module.exports = router;
