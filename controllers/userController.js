85b78a26155f5e4bbb2b63e2ffe43913920b1021
sanjeevyg <yogi.sjv@gmail.com>
2021-07-01 23:58:03 -0400

    user

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,20 +1,20 @@
 const User = require('./../models/userModel.js');
 
-exports.getAllUsers = async (req, res) => {
-    try { const user = await User.find()
-    res.status(500).json({
-        status: 'error',
-        message: 'This route is not yet defined',
-        data: {
-            user
-        }
-    })} catch(err) {
-        res.status(400).json({
-            status: 'fail',
-            message: 'Invalid data sent!'
-        })
-    }
-}
+// exports.getAllUsers = async (req, res) => {
+//     try { const user = await User.find()
+//     res.status(500).json({
+//         status: 'error',
+//         message: 'This route is not yet defined',
+//         data: {
+//             user
+//         }
+//     })} catch(err) {
+//         res.status(400).json({
+//             status: 'fail',
+//             message: 'Invalid data sent!'
+//         })
+//     }
+// }
 
 exports.deleteUser = (req, res) => {
     res.status(500).json({
